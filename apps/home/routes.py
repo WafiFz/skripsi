3a959106deb6470a06d1ed59d808e73a0e19f9d0
WafiFz <wafi20002@mail.unpad.ac.id>
2023-11-26 01:17:36 +0700

    feat: LOGIC - read file from request and convet to string

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -4,9 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from apps.home import blueprint
-from flask import render_template, request
+from flask import jsonify, render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+import fitz
+from io import BytesIO
 
 
 @blueprint.route('/index')
@@ -14,40 +16,53 @@ def index():
 
     return render_template('home/index.html', segment='index')
 
+@blueprint.route('/index', methods=['POST'])
+def index_post():
+    file = request.files['file']
 
-@blueprint.route('/<template>')
-@login_required
-def route_template(template):
+    # Check if the file has a PDF extension
+    if not file.filename.lower().endswith('.pdf'):
+        return jsonify({'success': False, 'output': 'Invalid file format. Please upload a PDF file.'}), 400
 
+    # Read the PDF file and convert it to a string
     try:
+        text = request_pdf_to_string(file)
 
-        if not template.endswith('.html'):
-            template += '.html'
+        # Lakukan sesuatu dengan text_content, misalnya, print atau kirim sebagai tanggapan
+        print(text)
 
-        # Detect the current page
-        segment = get_segment(request)
+        return jsonify({'success': True, 'output': text}), 400
+    except Exception as e:
+        return jsonify({'success': False, 'output': f'Error processing PDF: {str(e)}'}), 500
 
-        # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment)
+def pdf_to_string(file):
+    # Gunakan PyMuPDF untuk membaca teks dari PDF
+    pdf_document = fitz.open(file)
+    text_content = ""
 
-    except TemplateNotFound:
-        return render_template('home/page-404.html'), 404
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
+        text_content += page.get_text()
 
-    except:
-        return render_template('home/page-500.html'), 500
+    pdf_document.close()
 
+    return text_content
 
-# Helper - Extract current page name from request
-def get_segment(request):
-
+def request_pdf_to_string(file):
     try:
+        # Membaca file PDF dari respons permintaan
+        pdf_bytes = BytesIO(file.read())
+        
+        # Menggunakan PyMuPDF untuk membaca teks dari PDF
+        pdf_document = fitz.open(stream=pdf_bytes, filetype="pdf")
+        text_content = ""
 
-        segment = request.path.split('/')[-1]
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document[page_num]
+            text_content += page.get_text()
 
-        if segment == '':
-            segment = 'index'
+        pdf_document.close()
 
-        return segment
-
-    except:
-        return None
+        return text_content
+    except Exception as e:
+        return f'Error processing PDF: {str(e)}'
